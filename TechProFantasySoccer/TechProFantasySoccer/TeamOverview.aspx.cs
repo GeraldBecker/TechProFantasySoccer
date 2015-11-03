08f0d362b443de7e094e35c7707f33c72816430e
Gerald Becker <ger_0_1@hotmail.com>
2015-11-03 08:43:32 -0800

    Finished the team overview page by completing the sql pulls and getting the live data. The label fields are now being set.

--- TechProFantasySoccer/TechProFantasySoccer/TeamOverview.aspx.cs
+++ TechProFantasySoccer/TechProFantasySoccer/TeamOverview.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -27,9 +29,186 @@ namespace TechProFantasySoccer {
 
             GridView1.DataBind();*/
 
+            String strConnString = ConfigurationManager.ConnectionStrings["FantasySoccerConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(strConnString);
+            SqlCommand cmd = new SqlCommand();
 
-            
+            //Calculate fields
+            decimal totalPoints = 0;
+            decimal defenderPoints = 0;
+            decimal midfielderPoints = 0;
+            decimal strikerPoints = 0;
+
+
+            /*cmd.CommandText = 
+                "SELECT "+
+                "Players.PositionRef AS Position, "+
+                "SUM([PlayerStats].Goals) AS Goals, "+
+                "SUM([PlayerStats].Shots) AS Shots,"+
+                "SUM([PlayerStats].Assists) AS Assists,"+
+                "SUM([PlayerStats].MinPlayed) AS MinPlayed,"+
+                "SUM([PlayerStats].Fouls) AS Fouls,"+
+                "SUM([PlayerStats].YellowCards) AS YC,"+
+                "SUM([PlayerStats].RedCards) AS RC,"+
+                "SUM([PlayerStats].GoalsAllowed) AS GA,"+
+                "SUM([PlayerStats].SavesMade) AS Saves,"+
+                "dbo.CalculateTotalFantasyPoints(SUM([PlayerStats].Goals),"+
+								                "SUM([PlayerStats].Shots), "+
+								                "SUM([PlayerStats].Assists),"+
+								                "SUM([PlayerStats].MinPlayed),"+
+								                "SUM([PlayerStats].Fouls),"+
+								                "SUM([PlayerStats].YellowCards),"+
+								                "SUM([PlayerStats].RedCards),"+
+								                "SUM([PlayerStats].GoalsAllowed),"+
+								                "SUM([PlayerStats].SavesMade),"+
+								                "SUM([PlayerStats].CleanSheets),"+
+								                "Players.PositionRef) AS 'Total Fantasy Pts' "+
+                "FROM LineupHistory "+
+                "INNER JOIN Players ON LineupHistory.PlayerId = Players.PlayerId "+
+                "INNER JOIN PlayerStats ON PlayerStats.PlayerId = LineupHistory.PlayerId AND PlayerStats.Month = LineupHistory.Month "+
+                "WHERE LineupHistory.UserId = 1 "+
+                "GROUP BY Players.PositionRef";*/
+            cmd.CommandText =
+            "SELECT " +
+            "Players.PositionRef AS Position," +
+            "SUM([PlayerStats].Goals) AS Goals," +
+            "dbo.CalculateFantasyPointsByPosition(1, SUM([PlayerStats].Goals), Players.PositionRef) AS GoalsPts," +
+            "SUM([PlayerStats].Shots) AS Shots," +
+            "dbo.CalculateFantasyPointsByPosition(2, SUM([PlayerStats].Shots), Players.PositionRef) AS ShotsPts," +
+            "SUM([PlayerStats].Assists) AS Assists," +
+            "dbo.CalculateFantasyPointsByPosition(6, SUM([PlayerStats].Assists), Players.PositionRef) AS AssistsPts," +
+            "SUM([PlayerStats].MinPlayed) AS MinPlayed," +
+            "dbo.CalculateFantasyPointsByPosition(10, SUM([PlayerStats].MinPlayed), Players.PositionRef) AS MinPlayedPts," +
+            "SUM([PlayerStats].Fouls) AS Fouls," +
+            "dbo.CalculateFantasyPointsByPosition(3, SUM([PlayerStats].Fouls), Players.PositionRef) AS FoulsPts," +
+            "    SUM([PlayerStats].YellowCards) AS YC," +
+            "dbo.CalculateFantasyPointsByPosition(4, SUM([PlayerStats].YellowCards), Players.PositionRef) AS YCPts," +
+            "SUM([PlayerStats].RedCards) AS RC," +
+            "dbo.CalculateFantasyPointsByPosition(5, SUM([PlayerStats].RedCards), Players.PositionRef) AS RCPts," +
+            "SUM([PlayerStats].GoalsAllowed) AS GA," +
+            "dbo.CalculateFantasyPointsByPosition(8, SUM([PlayerStats].GoalsAllowed), Players.PositionRef) AS GAPts," +
+            "SUM([PlayerStats].SavesMade) AS Saves," +
+            "dbo.CalculateFantasyPointsByPosition(9, SUM([PlayerStats].SavesMade), Players.PositionRef) AS SavesPts," +
+            "SUM([PlayerStats].CleanSheets) AS CleanSheets," +
+            "dbo.CalculateFantasyPointsByPosition(7, SUM([PlayerStats].CleanSheets), Players.PositionRef) AS CleanSheetPts," +
+            "dbo.CalculateTotalFantasyPoints(SUM([PlayerStats].Goals)," +
+        "						SUM([PlayerStats].Shots), " +
+    "							SUM([PlayerStats].Assists)," +
+            "					SUM([PlayerStats].MinPlayed)," +
+            "					SUM([PlayerStats].Fouls)," +
+            "					SUM([PlayerStats].YellowCards)," +
+            "					SUM([PlayerStats].RedCards)," +
+            "					SUM([PlayerStats].GoalsAllowed)," +
+            "					SUM([PlayerStats].SavesMade)," +
+            "					SUM([PlayerStats].CleanSheets)," +
+            "					Players.PositionRef) AS 'Total Fantasy Pts' " +
+            "FROM LineupHistory " +
+            "INNER JOIN Players ON LineupHistory.PlayerId = Players.PlayerId " +
+            "INNER JOIN PlayerStats ON PlayerStats.PlayerId = LineupHistory.PlayerId AND PlayerStats.Month = LineupHistory.Month " +
+            "WHERE LineupHistory.UserId = 1 " +
+            "GROUP BY Players.PositionRef";
+
+
+            cmd.Connection = con;
+            try {
+                decimal goals = 0, goalPts = 0, shots = 0, shotPts = 0, assists = 0, assistPts = 0,
+                    minPlayed = 0, minPlayedPts = 0, fouls = 0, foulPts = 0, YC = 0, YCPts = 0,
+                    RC = 0, RCPts = 0, GC = 0, GCPts = 0, saves = 0, savePts = 0, cleanSheets = 0,
+                    cleanSheetPts = 0;
+
+                DataTable temp = new DataTable();
+                con.Open();
+                //GridView1.EmptyDataText = "No Records Found";
+                temp.Load(cmd.ExecuteReader());
+
+                DataRow[] foundRows;
+
+                for(int i = 1; i <= 4; i++) {
+                    foundRows = temp.Select("Position = " + i);
+                    if(foundRows.Length > 0) {
+                        if(i == 1)
+                            StrikersPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+                        else if(i == 2)
+                            MidfieldersPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+                        else if(i == 3)
+                            DefendersPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+                        else if(i == 4)
+                            GoaliesPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+
+                        goals += (int)foundRows[0]["Goals"];
+                        goalPts += (decimal)foundRows[0]["GoalsPts"];
+                        shots += (int)foundRows[0]["Shots"];
+                        shotPts += (decimal)foundRows[0]["ShotsPts"];
+                        assists += (int)foundRows[0]["Assists"];
+                        assistPts += (decimal)foundRows[0]["AssistsPts"];
+                        minPlayed += (int)foundRows[0]["MinPlayed"];
+                        minPlayedPts += (decimal)foundRows[0]["MinPlayedPts"];
+                        fouls += (int)foundRows[0]["Fouls"];
+                        foulPts += (decimal)foundRows[0]["FoulsPts"];
+                        YC += (int)foundRows[0]["YC"];
+                        YCPts += (decimal)foundRows[0]["YCPts"];
+                        RC += (int)foundRows[0]["RC"];
+                        RCPts += (decimal)foundRows[0]["RCPts"];
+                        GC += (int)foundRows[0]["GA"];
+                        GCPts += (decimal)foundRows[0]["GAPts"];
+                        saves += (int)foundRows[0]["Saves"];
+                        savePts += (decimal)foundRows[0]["SavesPts"];
+                        cleanSheets += (int)foundRows[0]["CleanSheets"];
+                        cleanSheetPts += (decimal)foundRows[0]["CleanSheetPts"];
+                    }
+                }
+
+                GoalsPtsLabel.Text = goals.ToString() + " = " + goalPts.ToString() + " pts";
+                ShotsPtsLabel.Text = shots.ToString() + " = " + shotPts.ToString() + " pts";
+                AssistsPtsLabel.Text = assists.ToString() + " = " + assistPts.ToString() + " pts";
+                MinPlayedPtsLabel.Text = minPlayed.ToString() + " = " + minPlayedPts.ToString() + " pts";
+                FoulsPtsLabel.Text = fouls.ToString() + " = " + foulPts.ToString() + " pts";
+                YCPtsLabel.Text = YC.ToString() + " = " + YCPts.ToString() + " pts";
+                RCPtsLabel.Text = RC.ToString() + " = " + RCPts.ToString() + " pts";
+                GCPtsLabel.Text = GC.ToString() + " = " + GCPts.ToString() + " pts";
+                SavesMadePtsLabel.Text = saves.ToString() + " = " + savePts.ToString() + " pts";
+                CleanSheetsPtsLabel.Text = cleanSheets.ToString() + " = " + cleanSheetPts.ToString() + " pts";
+
+
+
+                /*foundRows = temp.Select("Position = 2");
+                if(foundRows.Length > 0) {
+                    MidfieldersPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+                    goals += (decimal)foundRows[0]["Goals"];
+                    goalPts += (decimal)foundRows[0]["GoalsPts"];
+                }
+
+                foundRows = temp.Select("Position = 3");
+                if(foundRows.Length > 0) {
+                    DefendersPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+                }
+
+                foundRows = temp.Select("Position = 4");
+                if(foundRows.Length > 0) {
+                    GoaliesPtsLabel.Text = foundRows[0]["Total Fantasy Pts"].ToString() + " pts";
+                }*/
+
+            } finally {
+                con.Close();
+            }
+
+
+
+            /*cmd.CommandText = "SELECT Name FROM ScoringValues WHERE ScoringId = 3";
+
+            cmd.Connection = con;
+            try {
+                //GridView1.EmptyDataText = "No Records Found";
+                string text = (string)cmd.ExecuteScalar();
+                Label1.Text = text;
+            } finally {
+
+            }*/
         }
+
+
+
+
     }
 
 
